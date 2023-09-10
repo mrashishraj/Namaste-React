c484794a88049fd82860b288eb0c1937ee1161bd
mrashishraj <mr.ashishrajyadav@gmail.com>
2023-09-10 20:49:19 +0530

    assignment ep-03 completed

---
+++ Assignments/Ep-03/Coding/app.js
@@ -0,0 +1,25 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+    const reactElement = <h1>This is a react element</h1>
+    const ReactComponent2 = () =>(
+        <h1>This is other react component 🚀</h1>
+    )
+
+    const ReactComponent = () =>( <div>
+        <ReactComponent2/>
+        {reactElement}
+        <h1>This is a ReactComponent 🚀</h1> 
+    </div> )
+
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+    root.render(<ReactComponent/>)
+
+
+    #UI_c072decf-8438-4675-9aaa-4856ca94ee97 > div > div
+    #UI_c072decf-8438-4675-9aaa-4856ca94ee97 > div > div)
+
+
+    http://survey.webengage.com/ws/~27kkhcc?data(cuid)={{user['system']['cuid']|urlencode}}&data(MobileNumber)={{user['system']['phone']|urlencode}}&data(email)={{user['system']['email']|urlencode}}&data(product)={{event["custom"]["purchase"]["custom"]["product"]}}
+
+    {{event["custom"]["purchase"]["custom"]["product"]}}
