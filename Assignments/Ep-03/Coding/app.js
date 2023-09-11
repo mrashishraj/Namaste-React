1555df12b2a03e7e7209948d403de8dc564f1abb
mrashishraj <mr.ashishrajyadav@gmail.com>
2023-09-11 12:27:09 +0530

    ep-3 assgn done/modified

--- Assignments/Ep-03/Coding/app.js
+++ Assignments/Ep-03/Coding/app.js
@@ -1,25 +1,33 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-    const reactElement = <h1>This is a react element</h1>
+    const reactCreateElement = [
+        React.createElement("div",{id:"parent",className:"blue"},
+        React.createElement("div",{id:"child",className:"blue"},
+    [
+        React.createElement("h1",{id:"h1-tag"},"This is h1 tag from react.createElement"),
+        React.createElement("h2",{id:"h1-tag"},"this is the update  react.createElement")
+        ]
+        )),
+        React.createElement("div",{id:"parent",className:"blue"},
+        React.createElement("div",{id:"child",className:"blue"},
+        [React.createElement("h1",{id:"h1-tag"},"This is h1 tag react.createElement"),
+        React.createElement("h2",{id:"h1-tag"},"This is h2 tag react.createElement")
+    ]
+        ))]
+
+    const reactElement = <h1 className="pink">This is a react element</h1>
     const ReactComponent2 = () =>(
-        <h1>This is other react component 🚀</h1>
+        <h1 className="red">This is other react component 🚀</h1>
     )
 
     const ReactComponent = () =>( <div>
         <ReactComponent2/>
-        {reactElement}
-        <h1>This is a ReactComponent 🚀</h1> 
+        {reactElement} 
+        {reactCreateElement}
+        <h1 className="green">This is a ReactComponent 🚀</h1> 
     </div> )
 
     const root = ReactDOM.createRoot(document.getElementById("root"))
     root.render(<ReactComponent/>)
 
-
-    #UI_c072decf-8438-4675-9aaa-4856ca94ee97 > div > div
-    #UI_c072decf-8438-4675-9aaa-4856ca94ee97 > div > div)
-
-
-    http://survey.webengage.com/ws/~27kkhcc?data(cuid)={{user['system']['cuid']|urlencode}}&data(MobileNumber)={{user['system']['phone']|urlencode}}&data(email)={{user['system']['email']|urlencode}}&data(product)={{event["custom"]["purchase"]["custom"]["product"]}}
-
-    {{event["custom"]["purchase"]["custom"]["product"]}}
